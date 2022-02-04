62faa6b7c3d4a1008916249d6974e0ffdb923749
Xushnora <xushnoranishonova@gmail.com>
2022-02-04 07:20:12 +0500

    js changed

--- Js/main.js
+++ Js/main.js
@@ -6,12 +6,17 @@ let elMenuBtn = document.getElementById('menuBtn');
 let elNav= document.getElementById('headerNav');
 let elClose= document.getElementById('closeBtn');
 let logo= document.getElementById('logo');
+let darkRemov= document.getElementById('darkSite');
+
+
 
 elMenuBtn.addEventListener('click', function(){
 
     elMenuBtn.classList.add('test');
     elNav.classList.add('show');
     logo.classList.add('none');
+    darkRemov.classList.add('none');
+
 });
 
 
@@ -20,5 +25,6 @@ elClose.addEventListener('click', function(){
     elMenuBtn.classList.remove('test');
     elNav.classList.remove('show');
     logo.classList.remove('none');
+    darkRemov.classList.remove('none');
 
 });
